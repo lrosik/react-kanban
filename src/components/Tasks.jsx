94fd68d07f13585b44c3e16dd7052bde0cc9cb1d
Łukasz Rosik <lukiro96@gmail.com>
2019-01-29 21:03:17 +0100

    Added input and button for adding new tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,6 +3,7 @@ import Task from "./Task";
 
 class Tasks extends Component {
   state = {
+    newTask: "",
     tasks: [
       { id: 1, task: "Task 1" },
       { id: 2, task: "Task 2" },
@@ -14,6 +15,19 @@ class Tasks extends Component {
   render() {
     return (
       <div>
+        <input
+          placeholder="Task name"
+          className="form-control m-2 w-25"
+          type="text"
+          value={this.state.newTask}
+          onChange={e => this.handleAddInput(e)}
+        />
+        <button
+          className="btn btn-primary m-2"
+          onClick={() => this.handleAdd()}
+        >
+          Add
+        </button>
         {this.state.tasks.map(task => (
           <Task key={task.id} task={task} onDone={this.handleDone} />
         ))}
@@ -22,9 +36,32 @@ class Tasks extends Component {
   }
 
   handleDone = id => {
-    const tasks = this.state.tasks.filter(t => t.id != id);
+    const tasks = this.state.tasks.filter(t => t.id !== id);
     this.setState({ tasks });
   };
+
+  handleAddInput = e => {
+    this.setState({ newTask: e.target.value });
+  };
+
+  handleAdd() {
+    const newId = this.findMaxId(this.state.tasks) + 1;
+    const newTask = { id: newId, task: this.state.newTask };
+    this.setState(pState => ({
+      tasks: [...pState.tasks, newTask]
+    }));
+  }
+
+  findMaxId(tasks) {
+    let max = tasks[0].id;
+
+    tasks.forEach(element => {
+      max = max < element.id ? element.id : max;
+    });
+
+    console.log(max);
+    return max;
+  }
 }
 
 export default Tasks;
